ce061cf7e7d0d58ab936047ffeb7432027d24f9d
Al Ameen A R <40757425+amrameen769@users.noreply.github.com>
2020-01-12 19:35:04 +0530

    alpha v0.1

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -11,8 +11,8 @@ class Header extends Component {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#features">Address Book</Nav.Link>
+                        <Nav.Link href="#pricing">Sponsors</Nav.Link>
                         <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
